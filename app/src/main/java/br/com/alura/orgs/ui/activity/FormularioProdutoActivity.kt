0c3acac83a981e86d36c9cec12791bafbc446fe2
vinnybrito <viny.rbrito15@hotmail.com>
2024-02-24 17:06:37 -0300

    Atualizando repositório

--- app/src/main/java/br/com/alura/orgs/ui/activity/FormularioProdutoActivity.kt
+++ app/src/main/java/br/com/alura/orgs/ui/activity/FormularioProdutoActivity.kt
@@ -1,25 +1,43 @@
 package br.com.alura.orgs.ui.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import br.com.alura.orgs.R
+import br.com.alura.orgs.model.Produto
+import java.math.BigDecimal
 
 class FormularioProdutoActivity :
     AppCompatActivity(R.layout.activity_formulario_produto) {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        val campoNome = findViewById<EditText>(R.id.nome)
-        val nome = campoNome.text.toString()
-
-        Log.i("FormularioProduto", "onCreate: $nome")
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
 
         val botaoSalvar = findViewById<Button>(R.id.butao_salvar)
-        botaoSalvar.setOnClickListener {  }
+        botaoSalvar.setOnClickListener {
+            val campoNome = findViewById<EditText>(R.id.nome)
+            val nome = campoNome.text.toString()
+
+            val campoDescricao = findViewById<EditText>(R.id.descricao)
+            val descricao = campoDescricao.text.toString()
+
+            val camporValor = findViewById<EditText>(R.id.valor)
+            val valor = camporValor.text.toString()
+
+            Produto(
+                nome = nome,
+                descricao = descricao,
+                valor = BigDecimal(valor)
+            )
+
+            Log.i("FormularioProduto", "onCreate: $nome")
+        }
 
 
     }
